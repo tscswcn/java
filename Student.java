9e36e52df261380de76eafadc97d518974c6ed39
luopeng <755200@qq.com>
2019-09-15 17:23:24 +0800

    java 集合操作
    java 集合操作

--- Student.java
+++ Student.java
@@ -1,48 +1,18 @@
+package new1;
+
 public class Student {
-    String name="";
-    int age;
-    int sno;
-    /*
-    public Student()
-    {
-     System.out.printf("没有参数");
-    }
-    */
-    public Student(int age)
-    {
-        System.out.printf("age");
-        this.age=age;
-    }
-    public Student(String name)
-    {
-        this.name=name;
-        System.out.printf("name");
+    public String id;
+    public String name;
+    public Student(String id, String name){
+        this.id = id;
+        this.name = name;
     }
 
-    public Student() {
-
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
     }
-
-    public int setAge(int age) {
-        if (age<=130 && age>=0)
-        this.age=age;
-        else System.out.print("年龄在在0到130之间");
-        return age;
-    }
-    public int getAge(int age) {
-
-        return this.age;
-    }
-
-    public static void main(String args[])
- {
-      Student student1=new Student(21);
-      System.out.print(student1.age);
-      Student student2=new Student("luopeng");
-      System.out.printf(student2.name);
-     Student student3=new Student();
-     System.out.print(student3.age);
-     System.out.println(student3.setAge(133));
- }
-
 }
