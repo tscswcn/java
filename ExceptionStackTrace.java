afc2a97c79f92d25a588bfbbbd89d86c29bd59a8
luopeng <755200@qq.com>
2019-09-16 08:37:18 +0800

    ExceptionStackTrace.java
    ExceptionStackTrace.java

---
+++ ExceptionStackTrace.java
@@ -0,0 +1,19 @@
+package new1;
+
+public class ExceptionStackTrace {
+    private static void method1() {
+        method2();
+    }
+
+    private static void method2() {
+        throw new NullPointerException();
+    }
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception e) {
+            //¥Ú”°∂—’ªπÏº£
+            e.printStackTrace();
+        }
+    }
+}
